7a69b4d049ff9d845845b70ff48b5f233d49038a
NuoriM <34687879+NuoriM@users.noreply.github.com>
2022-12-30 00:10:05 -0300

    Criação do navbar

--- src/components/VNav.vue
+++ src/components/VNav.vue
@@ -1,19 +1,52 @@
 <template>
 	<div class="v-nav">
+		<div class="v-nav-title">GESTAOGASTOS</div>
 		<div class="v-nav-itens">
-			<div>Item 1</div>
-			<div>Item 2</div>
-			<div>Item 3</div>
+			<span><router-link to="/">Home</router-link></span>
+			<span>Serviços</span>
+			<span>Sobre</span>
 		</div>
 	</div>
 </template>
 <style>
+	@import url('https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Baloo+2&display=swap');
+
 	.v-nav {
+		background: #1a1a1a;
 		display: flex;
+		align-items: center;
+		padding: 8px;
+	}
+
+	.v-nav-title {
+		font-family: 'Archivo Black', sans-serif;
+		color: #F1F6F5;
 	}
 
 	.v-nav-itens {
 		display: flex;
+		flex: 1 1 auto;
+		justify-content: center;
+		gap: 16px;
+	}
+
+	.v-nav-itens span a{
+		font-family: 'Baloo 2', cursive;
+		text-decoration: none !important;
+		padding: 4px 16px;
+		background: #ececec;
+		color: #1a1a1a;
+		border-radius: 32px;
+	}
+
+	.v-nav-itens span a{
+		font-family: 'Baloo 2', cursive;
+		text-decoration: none !important;
+		padding: 4px 16px;
+		background: #ececec;
+		color: #1a1a1a;
+		border-radius: 32px;
 	}
 </style>
 <script>
